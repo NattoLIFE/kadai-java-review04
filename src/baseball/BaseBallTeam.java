b8543ec403692d9dd73f47b5eedb77ef39a2c2ec
marim <marim@DESKTOP-V1RHNCF>
2024-03-07 23:03:54 +0900

    kadai-java-review03

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,24 @@
+package baseball;
+
+class BaseBallTeam {
+    private String name;
+    private int win;
+    private int lose;
+    private int draw;
+
+    public BaseBallTeam(String name, int win, int lose, int draw) {
+        this.name = name;
+        this.win = win;
+        this.lose = lose;
+        this.draw = draw;
+    }
+
+    public double getRate() {
+        return (double) win / (win + lose); // 勝率の計算
+    }
+
+    public void report() {
+        System.out.println(this.name + "の2022年の成績は" + this.win + "勝" + this.lose + "敗" + this.draw + "分、" + "勝率は"
+                + getRate() + "です。");
+    }
+}
